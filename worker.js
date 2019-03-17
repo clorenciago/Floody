78b3c552a54237e9c008795ca35329423ef0b0d2
clorenciago <46843985+clorenciago@users.noreply.github.com>
2019-03-17 12:52:07 +0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,25 +1,11 @@
-const first = document.querySelector('#number1');
-const second = document.querySelector('#number2');
-console.log("Freedom");
-const result = document.querySelector('.result');
-
-if (window.Worker) {
-	const myWorker = new Worker("worker.js");
-
-	first.onchange = function() {
-	  myWorker.postMessage([first.value,second.value]);
-	  console.log('Message posted to worker');
-	}
-
-	second.onchange = function() {
-	  myWorker.postMessage([first.value,second.value]);
-	  console.log('Message posted to worker');
-	}
-
-	myWorker.onmessage = function(e) {
-		result.textContent = e.data;
-		console.log('Message received from worker');
-	}
-} else {
-	console.log('Your browser doesn\'t support web workers.')
+onmessage = function(e) {
+  console.log('Worker: Message received from main script');
+  let result = e.data[0] * e.data[1];
+  if (isNaN(result)) {
+    postMessage('Please write two numbers');
+  } else {
+    let workerResult = 'Result: ' + result;
+    console.log('Worker: Posting message back to main script');
+    postMessage(workerResult);
+  }
 }
